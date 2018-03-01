2a686943bd3f968dbbdf0de3cbb24519edb9f2e4
oleg291291@mail.ru <oleg291291@mail.ru>
2018-03-01 17:17:45 +0300

    something but not tested

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    //this.inValue = initialValue;
+    
+    this.value = initialValue; // тут ответ
+    this.stackArr = [];
+
+    this.calc = function calc(){
+      this.stackArr.push(initialValue);
+      //mult
+      var multStr = "*"
+      var multIndex = this.stackArr.indexOf(multStr);
+      while(multIndex != -1){
+        var multed = this.stackArr[multIndex - 1] * this.stackArr[multIndex + 1]
+        this.stackArr[multIndex - 1] = multed;
+        this.stackArr.splice(multIndex, 2);
+        multIndex = this.stackArr.indexOf(multStr);
+      }
+      //sum
+      var plusStr = '+'
+      var sumIndex = this.stackArr.indexOf(plusStr);
+      while(sumIndex != -1){
+        var summed = this.stackArr[sumIndex - 1] * this.stackArr[sumIndex + 1]
+        this.stackArr[sumIndex - 1] = summed;
+        this.stackArr.splice(sumIndex, 2);
+        sumIndex = this.stackArr.indexOf(plusStr);
+      }
+      this.value = this.stackArr[0];
+      //return this.stackArr[0]
+    }
+   // return initialValue;
   }
 
   add(number) {
     // your implementation
+    this.stackArr.push('+')
+    this.stackArr.push(number)
+    this.calculate();
   }
   
   subtract(number) {
@@ -13,6 +45,9 @@ class SmartCalculator {
 
   multiply(number) {
     // your implementation
+    this.stackArr.push('*')
+    this.stackArr.push(number)
+    this.calculate();
   }
 
   devide(number) {
@@ -22,6 +57,8 @@ class SmartCalculator {
   pow(number) {
     // your implementation
   }
+  
 }
 
 module.exports = SmartCalculator;
+
