b4d928ad2e03d7b0ffde3e39a878800fdec33f5d
oleg291291@mail.ru <oleg291291@mail.ru>
2018-03-02 16:38:21 +0300

    now it's number-like

--- src/index.js
+++ src/index.js
@@ -1,64 +1,40 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
-    //this.inValue = initialValue;
     
-    this.value = initialValue; // тут ответ
-    this.stackArr = [];
-
-    this.calc = function calc(){
-      this.stackArr.push(initialValue);
-      //mult
-      var multStr = "*"
-      var multIndex = this.stackArr.indexOf(multStr);
-      while(multIndex != -1){
-        var multed = this.stackArr[multIndex - 1] * this.stackArr[multIndex + 1]
-        this.stackArr[multIndex - 1] = multed;
-        this.stackArr.splice(multIndex, 2);
-        multIndex = this.stackArr.indexOf(multStr);
-      }
-      //sum
-      var plusStr = '+'
-      var sumIndex = this.stackArr.indexOf(plusStr);
-      while(sumIndex != -1){
-        var summed = this.stackArr[sumIndex - 1] * this.stackArr[sumIndex + 1]
-        this.stackArr[sumIndex - 1] = summed;
-        this.stackArr.splice(sumIndex, 2);
-        sumIndex = this.stackArr.indexOf(plusStr);
-      }
-      this.value = this.stackArr[0];
-      //return this.stackArr[0]
-    }
-   // return initialValue;
   }
 
+
   add(number) {
-    // your implementation
-    this.stackArr.push('+')
-    this.stackArr.push(number)
-    this.calculate();
+    
+    return this;
+    
   }
   
   subtract(number) {
-    // your implementation
+   
+    return this;
   }
 
   multiply(number) {
-    // your implementation
-    this.stackArr.push('*')
-    this.stackArr.push(number)
-    this.calculate();
+   
+    return this;
   }
 
   devide(number) {
-    // your implementation
+   
+    return this;
   }
 
   pow(number) {
-    // your implementation
+   
+    return this;
   }
   
 }
+
+SmartCalculator.prototype.valueOf = function() { return 66 };
+
 
 module.exports = SmartCalculator;
 
