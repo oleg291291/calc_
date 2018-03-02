d6c411bfa01a287e14fdbaa628832198fe27890c
oleg291291@mail.ru <oleg291291@mail.ru>
2018-03-02 18:39:12 +0300

    ready!

--- src/index.js
+++ src/index.js
@@ -1,40 +1,12 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
-    this.result = 0; // тут ответ
+    this.result = 0; 
     this.stackArr = [initialValue];
-
-    // this.calc = function calc(){
-    //   // this.stackArr.push(initialValue);
-    //   //mult
-    //   var multStr = "*"
-    //   var multIndex = this.stackArr.indexOf(multStr);
-    //   while(multIndex != -1){
-    //     var multed = this.stackArr[multIndex - 1] * this.stackArr[multIndex + 1]
-    //     this.stackArr[multIndex - 1] = multed;
-    //     this.stackArr.splice(multIndex, 2);
-    //     multIndex = this.stackArr.indexOf(multStr);
-    //   }
-    //   //sum
-    //   var plusStr = '+'
-    //   var sumIndex = this.stackArr.indexOf(plusStr);
-    //   while(sumIndex != -1){
-    //     var summed = this.stackArr[sumIndex - 1] + this.stackArr[sumIndex + 1]
-    //     this.stackArr[sumIndex - 1] = summed;
-    //     this.stackArr.splice(sumIndex, 2);
-    //     sumIndex = this.stackArr.indexOf(plusStr);
-    //   }
-    //   this.result = this.stackArr[0];
-     
-    // }
-
   }
 
  calc(){
-      // this.stackArr.push(initialValue);
-      //mult
-    //   this.result = initialValue; // тут ответ
-    // this.stackArr = [initialValue];
+    
 
       var calcArr = this.stackArr.slice();
       
@@ -47,15 +19,6 @@ class SmartCalculator {
               calcArr.splice(powIndex+2, 2);
             }
           }
-
-        
-
-        // if(calcArr[powIndex + 1] == 1 && calcArr[powIndex + 2] == "pow" && calcArr[powIndex + 3] == 2 && calcArr[powIndex + 4] == "pow" && calcArr[powIndex + 5] == 2){
-        //   calcArr.splice(powIndex+2, 4);
-        // }
-        // if(calcArr[powIndex + 1] == 1 && calcArr[powIndex + 2] == "pow" && calcArr[powIndex + 3] == 2){
-        //   calcArr.splice(powIndex+2, 2);
-        // }
 
         var powed = Math.pow(calcArr[powIndex - 1], calcArr[powIndex + 1]);
         calcArr[powIndex - 1] = powed;
